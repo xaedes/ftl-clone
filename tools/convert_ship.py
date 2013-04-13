1c2220be4e411e600ab85aa293ded5f4ff1cd25b
xaedes <xaedes@googlemail.com>
2013-04-13 17:08:45 +0200

    added tool to print json data of ship from original ftl data

---
+++ tools/convert_ship.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+
+import sys
+import array
+from struct import unpack
+from os.path import dirname, basename, exists
+from os import makedirs
+import json
+
+assert len(sys.argv) >= 2
+
+filename = sys.argv[1]
+
+keys = {"ROOM":["id", "x", "y", "w", "h"],"DOOR":["x", "y", "id1", "id2","direction"]}
+
+with open(filename, "r") as f:
+    lines = [line.strip() for line in f]
+    
+    i = 0
+    objs = {"ROOM":[],"DOOR":[]}
+    while i < len(lines):
+        if lines[i] in keys.keys():
+            obj = dict( zip( keys[lines[i]], map( int, lines[i+1:i+1+len(keys[lines[i]])] ) ) )
+            objs[lines[i]].append(obj)
+            i += len(keys[lines[i]])
+        i += 1
+        
+    print json.dumps({"rooms":objs["ROOM"],"doors":objs["DOOR"]}, indent=4, separators=(',', ': ')).replace('"','')
+    exit(0)
+    

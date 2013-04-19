10b7d9886acbf8a4ae8342dcd4c2a30bbb9925f6
xaedes <xaedes@googlemail.com>
2013-04-19 16:30:34 +0200

    extract will now extract all data

--- tools/extract.py
+++ tools/extract.py
@@ -1,28 +1,38 @@
 #!/usr/bin/env python2
 
 from struct import unpack
-from os.path import dirname, basename, exists
+from os.path import dirname, basename, exists, normpath, join
 from os import makedirs
 
+# Extracts all ftl resources in current directory into current directory
 
-with open("resource.dat", "rb") as f: 
-	f.seek(4) # Seek to begin
 
-	begin = unpack('<I', f.read(4))[0]
-	print begin
-	f.seek(begin)
-	
-	while(True):
-		filesize = unpack('<I', f.read(4))[0]
-		filename_length = unpack('<I', f.read(4))[0]
+def extract(filename):
+	with open(filename, "rb") as f: 
+		f.seek(4) # Seek to begin
 
-		filename = f.read(filename_length)
-		data = f.read(filesize)
+		begin = unpack('<I', f.read(4))[0]
+		print begin
+		f.seek(begin)
+		
+		while(True):
+			filesize = f.read(4)
+			if(filesize == ""):
+				break
+			filesize = unpack('<I', filesize)[0]
+			filename_length = unpack('<I', f.read(4))[0]
 
-		if not exists( dirname(filename) ):
-			makedirs( dirname(filename) )
+			filename = f.read(filename_length)
+			data = f.read(filesize)
 
-		with open(filename, "wb") as output: 
-			output.write(data)
+			if not exists( dirname(filename) ):
+				makedirs( dirname(filename) )
 
-		print filesize, filename_length, filename
+			with open(filename, "wb") as output: 
+				output.write(data)
+
+			print filesize, filename_length, filename
+
+
+extract("resource.dat")
+extract("data.dat")
